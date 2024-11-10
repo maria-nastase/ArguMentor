e2fb80c7e06051da17193ee4ec201f2a6d4dc0e1
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2024-11-10 05:36:32 -0500

    dropdowns

--- debate/src/app/components/SpeechBubble.tsx
+++ debate/src/app/components/SpeechBubble.tsx
@@ -11,12 +11,12 @@ export default function SpeechBubble({arr, index}){
 
     const classN = "flex w-full "+ (arr.isUser ? "flex-row-reverse mt-[10em] relative" : "flex-row")
     const classNN = "mt-6 max-w-[75%] " + (arr.isUser ? "bg-red-500":"bg-blue-500");
-    const classArrow = "absolute -bottom-4 right-8";
+    const classArrow = "absolute -bottom-5 right-10 p-1 bg-blue-900";
 
     return <div key={index} className={classN}>
         <div className={classNN}>{arr.text} 
         {showSugg ? <div className="bg-green-900"><span className="font-bold">Suggestion: </span>{arr.suggestion}</div> : ""}
-        {showSugg ? <FaChevronUp onClick={handleClick} className={classArrow}></FaChevronUp>:<FaChevronDown onClick={handleClick} className={classArrow}></FaChevronDown>}
-        {arr.isUser ? <div className="bg-orange-500 absolute -bottom-2 -right-2">{arr.score < 0 ? "..." : arr.score}</div> : ""}</div>
+        {arr.isUser ? <div onClick={handleClick} className={classArrow}>{showSugg ? <FaChevronUp></FaChevronUp>:<FaChevronDown></FaChevronDown>}</div> : ""}
+        {arr.isUser ? <div className="bg-orange-500 absolute p-1 -bottom-7 -right-2">{arr.score < 0 ? "..." : arr.score}</div> : ""}</div>
     </div>;
 }
