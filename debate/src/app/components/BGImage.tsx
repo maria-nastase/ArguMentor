4ef41475a8300cdcbfe8f701c8fe8ae4a1bd5908
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2024-11-10 00:37:40 -0500

    credit the creator + set z-index

--- debate/src/app/components/BGImage.tsx
+++ debate/src/app/components/BGImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function BGImage() {
     return (<img
-        className="w-full h-full"
+        className="w-full h-full absolute top:0 left:0 z-[-100]"
         src="/8533.svg"
         alt="Debating characters"
     />);
--- debate/src/app/page.tsx
+++ debate/src/app/page.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import BGImage from './components/BGImage';
 
 
-export default function Home(){
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <AppStateProvider>
@@ -14,10 +14,11 @@ export default function Home(){
           <h1>Debate Coach</h1>
           <ResponseLog></ResponseLog>
           <GPTInput></GPTInput>
-          <BGImage></BGImage>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <a href="http://www.freepik.com">Designed by pch.vector / Freepik</a>
         </footer>
+        <BGImage></BGImage>
       </AppStateProvider>
     </div >
   );
