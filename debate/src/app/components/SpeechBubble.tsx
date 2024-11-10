1e494dc6fff4d638f06853ff7fce50437f07aeb4
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2024-11-10 05:53:44 -0500

    title bigger

--- debate/src/app/components/SpeechBubble.tsx
+++ debate/src/app/components/SpeechBubble.tsx
@@ -9,12 +9,12 @@ export default function SpeechBubble({arr, index}){
         setShowSug(c => !c);
     }
 
-    const classN = "flex w-full "+ (arr.isUser ? "flex-row-reverse mt-[10em] relative" : "flex-row")
-    const classNN = "mt-6 max-w-[75%] " + (arr.isUser ? "bg-red-500":"bg-blue-500");
+    const containerClasses = "flex w-full "+ (arr.isUser ? "flex-row-reverse mt-[10em] relative" : "flex-row")
+    const bubbleClasses = " rounded-md p-1 mt-6 max-w-[75%] " + (arr.isUser ? "bg-red-500":"bg-blue-500");
     const classArrow = "absolute -bottom-5 right-10 p-1 bg-blue-900";
 
-    return <div key={index} className={classN}>
-        <div className={classNN}>{arr.text} 
+    return <div key={index} className={containerClasses}>
+        <div className={bubbleClasses}>{arr.text} 
         {showSugg ? <div className="bg-green-900"><span className="font-bold">Suggestion: </span>{arr.suggestion}</div> : ""}
         {arr.isUser ? <div onClick={handleClick} className={classArrow}>{showSugg ? <FaChevronUp></FaChevronUp>:<FaChevronDown></FaChevronDown>}</div> : ""}
         {arr.isUser ? <div className="bg-orange-500 absolute p-1 -bottom-7 -right-2">{arr.score < 0 ? "..." : arr.score}</div> : ""}</div>
--- debate/src/app/page.tsx
+++ debate/src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <AppStateProvider>
-      <h1>Debate Coach</h1>
+      <h1 className='text-9xl'>Debate Coach</h1>
         <main className="flex flex-col gap-8 w-full row-start-2 items-center sm:items-start">
           <ResponseLog></ResponseLog>            
         <BGImage></BGImage>
         </main>
-        <footer className="absolute w-full bottom-0">
+        <footer className="w-full bottom-0">
           <a href="http://www.freepik.com">Designed by pch.vector / Freepik</a>
         </footer>
       </AppStateProvider>
